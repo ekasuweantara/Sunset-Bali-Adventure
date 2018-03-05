ee51b77590cc4d0d6330c04173136c01437f37cc
Agus Suarya <agus.suarya@spinroam.com>
2018-03-05 21:46:06 +0800

    perbaikan menu header

---
+++ application/views/v_booking.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+  </head>
+  <body>
+    <div id="fh5co-wrapper">
+      <div id="fh5co-page">
+
+        <!-- end:fh5co-header -->
+        <aside id="fh5co-hero" class="js-fullheight">
+          <div class="flexslider js-fullheight">
+            <ul class="slides">
+              <li style="background-image: url(<?php echo base_url('/assets/images/slider1.jpg'); ?>);">
+                <div class="overlay-gradient"></div>
+                <div class="container">
+                  <div class="col-md-12 col-md-offset-0 text-center slider-text">
+                    <div class="slider-text-inner js-fullheight">
+                      <div class="desc">
+                        <p><span>Sunset Bali Adventure</span></p>
+                        <h2>Tag line about ride</h2>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style="background-image: url(<?php echo base_url('/assets/images/slider2.jpg'); ?>);">
+                <div class="overlay-gradient"></div>
+                <div class="container">
+                  <div class="col-md-12 col-md-offset-0 text-center slider-text">
+                    <div class="slider-text-inner js-fullheight">
+                      <div class="desc">
+                        <p><span>Sunset Bali Adventure</span></p>
+                        <h2>Tag line about fun</h2>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style="background-image: url(<?php echo base_url('/assets/images/slider3.jpg'); ?>);">
+                <div class="overlay-gradient"></div>
+                <div class="container">
+                  <div class="col-md-12 col-md-offset-0 text-center slider-text">
+                    <div class="slider-text-inner js-fullheight">
+                      <div class="desc">
+                        <p><span>Sunset Bali Adventure</span></p>
+                        <h2>Tag line about explore</h2>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              </ul>
+            </div>
+        </aside>
+
+        <!-- <br><br><br><br><br><br><br><br><br><br><br> -->
+        <div id="hotel-facilities">
+      		<div class="container">
+      			<div class="row">
+      				<div class="col-md-12">
+      					<div class="section-title text-center">
+      						<h2>Sunset Bali Facilities</h2>
+      					</div>
+      				</div>
+      			</div>
+
+      			<div id="tabs">
+      				<nav class="tabs-nav">
+                <a href="#" class="active" data-tab="tab1">
+                  <span>ATV Adventure</span>
+                </a>
+      					<a href="#" data-tab="tab2">
+      						<span>River Tubing</span>
+      					</a>
+      					<a href="#" data-tab="tab3">
+      						<span>Swing</span>
+      					</a>
+      					<a href="#" data-tab="tab4">
+      						<span>Cycling</span>
+      					</a>
+      					<a href="#" data-tab="tab5">
+      						<span>SPA & Reflexy</span>
+      					</a>
+                <a href="#" data-tab="tab6">
+      						<span>Restaurant</span>
+      					</a>
+      				</nav>
+      				<div class="tab-content-container">
+      					<div class="tab-content active show" data-tab-content="tab1">
+      						<div class="container">
+      							<div class="row">
+      								<div class="col-md-6">
+      									<img src="<?php echo base_url('/assets/images/tab_img_1.jpg'); ?>" class="img-responsive" alt="Image">
+      								</div>
+      								<div class="col-md-6">
+      									<h3 class="heading">ATV Adventure</h3>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+      									<p class="service-hour">
+      										<span>Service Hours</span>
+      										<strong>7:30 AM - 8:00 PM</strong>
+      									</p>
+                        <p><a href="#" class="btn btn-primary btn-lg">Read More</a></p>
+      								</div>
+      							</div>
+      						</div>
+      					</div>
+      					<div class="tab-content" data-tab-content="tab2">
+      						<div class="container">
+      							<div class="row">
+      								<div class="col-md-6">
+      									<img src="<?php echo base_url('/assets/images/tab_img_2.jpg'); ?>" class="img-responsive" alt="Image">
+      								</div>
+      								<div class="col-md-6">
+      									<h3 class="heading">River Tubing</h3>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+      									<p class="service-hour">
+      										<span>Service Hours</span>
+      										<strong>7:30 AM - 8:00 PM</strong>
+      									</p>
+                        <p><a href="#" class="btn btn-primary btn-lg">Read More</a></p>
+      								</div>
+      							</div>
+      						</div>
+      					</div>
+      					<div class="tab-content" data-tab-content="tab3">
+      						<div class="container">
+      							<div class="row">
+      								<div class="col-md-6">
+      									<img src="<?php echo base_url('/assets/images/tab_img_3.jpg'); ?>" class="img-responsive" alt="Image">
+      								</div>
+      								<div class="col-md-6">
+      									<h3 class="heading">Swing</h3>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+      									<p class="service-hour">
+      										<span>Service Hours</span>
+      										<strong>7:30 AM - 8:00 PM</strong>
+      									</p>
+                        <p><a href="#" class="btn btn-primary btn-lg">Read More</a></p>
+      								</div>
+      							</div>
+      						</div>
+      					</div>
+      					<div class="tab-content" data-tab-content="tab4">
+      						<div class="container">
+      							<div class="row">
+      								<div class="col-md-6">
+      									<img src="<?php echo base_url('/assets/images/tab_img_4.jpg'); ?>" class="img-responsive" alt="Image">
+      								</div>
+      								<div class="col-md-6">
+      									<h3 class="heading">Cycling</h3>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+      									<p class="service-hour">
+      										<span>Service Hours</span>
+      										<strong>7:30 AM - 8:00 PM</strong>
+      									</p>
+                        <p><a href="#" class="btn btn-primary btn-lg">Read More</a></p>
+      								</div>
+      							</div>
+      						</div>
+      					</div>
+      					<div class="tab-content" data-tab-content="tab5">
+      						<div class="container">
+      							<div class="row">
+      								<div class="col-md-6">
+      									<img src="<?php echo base_url('/assets/images/tab_img_5.jpg'); ?>" class="img-responsive" alt="Image">
+      								</div>
+      								<div class="col-md-6">
+      									<h3 class="heading">SPA & Reflexy</h3>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+      									<p class="service-hour">
+      										<span>Service Hours</span>
+      										<strong>7:30 AM - 8:00 PM</strong>
+      									</p>
+                        <p><a href="#" class="btn btn-primary btn-lg">Read More</a></p>
+      								</div>
+      							</div>
+      						</div>
+      					</div>
+      					<div class="tab-content" data-tab-content="tab6">
+      						<div class="container">
+      							<div class="row">
+      								<div class="col-md-6">
+      									<img src="<?php echo base_url('/assets/images/tab_img_6.jpg'); ?>" class="img-responsive" alt="Image">
+      								</div>
+      								<div class="col-md-6">
+      									<h3 class="heading">Restaurant</h3>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+      									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+      									<p class="service-hour">
+      										<span>Service Hours</span>
+      										<strong>7:30 AM - 8:00 PM</strong>
+      									</p>
+                        <p><a href="#" class="btn btn-primary btn-lg">Read More</a></p>
+      								</div>
+      							</div>
+      						</div>
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+
+
+        <div class="row">
+          <h1>hay</h1>
+        </div>
+
+      </div>
+    </div>
+  </body>
+</html>
